d5a84a7870f78f75dd5b45d1d048bf5b91b439cb
Gabriel-AB <gabriel.a07b@gmail.com>
2021-01-25 11:36:40 -0300

    correção

--- FunctionDrawer.py
+++ FunctionDrawer.py
@@ -33,28 +33,3 @@ class FunctionDrawer(object):
   def draw(self, **kwargs):
     plt.pause(0.1)
     plt.draw()
-
-
-# Some functions to test
-def sphere(x):
-  return tf.reduce_sum(x*x)
-
-def ackley(x, a=20, b=0.2, c=2*np.math.pi):
-  if isinstance(x, tf.Tensor):
-    d = x.shape.as_list()[0]
-  else:
-    d = len(x)
-  return -a * tf.exp(-b * tf.sqrt(tf.reduce_sum(x*x)/d)) - \
-            tf.exp(tf.reduce_sum(tf.cos(c*x))/d) + a + np.math.e
-
-def griewank(x):
-  sum = x**2 / 4000
-  prod = tf.cos(np.array([num/tf.sqrt(i) for i, num in enumerate(x, start=1)]))
-  return tf.reduce_sum(sum) - tf.reduce_prod(prod) + 1
-
-def rastrigin(x):
-  if isinstance(x, tf.Tensor):
-    d = x.shape.as_list()[0]
-  else:
-    d = len(x)
-  return 10*d + tf.reduce_sum(x**2 - 10*tf.cos(x * 2 * np.math.pi))
