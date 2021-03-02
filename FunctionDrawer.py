a4d43502d01bf14e9b16d5f34bffac6191d8bc31
Gabriel-AB <gabriel.a07b@gmail.com>
2021-03-02 11:33:34 -0300

    FunctionDrawer otimizado - verificação de tipo removida das funções - funções agora podem receber 'batches' - troca range para tf.range

--- FunctionDrawer.py
+++ FunctionDrawer.py
@@ -2,35 +2,56 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-
 class FunctionDrawer(object):
-    def __init__(self, function, range, resolution=80):
-        self.create_mesh(function, range, resolution)
+    """ Draw Optimization functions implemented with tensorflow
+    function:
+        tensorflow function to draw
+    domain: 
+        hypercube tuple with minimum and maximum values for the domain
+        ex: (-10, 10)
+            (-5.12, 5.12)
+    resolution:
+        integer representing quality of render 
+        note: bigger numbers make interactive window slow
+    """
+    def __init__(self, function=None, domain=(-10, 10), resolution=80):
+        if not function:
+            def linear(x):
+                return tf.reduce_sum(x, axis=0)
+            function = linear
+        self.set_mesh(function, domain, resolution)
 
-    def create_mesh(self, function, range, resolution):
+    #  Set internal mesh to a new function
+    def set_mesh(self, function, domain:tuple, resolution=80):
+
         self.function = function
         self.ax = plt.subplot(projection='3d')
-        self.range = range
-        self.res = resolution
+        self.domain = domain
+        self.quality = resolution
 
         # creating mesh
-        x = y = np.linspace(-self.range, self.range, self.res)
+        x = y = np.linspace(domain[0], domain[1], self.quality)
         X, Y = np.lib.meshgrid(x, y)
 
-        zs = [self.function(np.array([x, y])) for x, y in zip(np.ravel(X), np.ravel(Y))]
+        zs = [np.ravel(X), np.ravel(Y)]
+        zs = tf.constant(zs)
+        zs = self.function(zs).numpy()
 
-        if isinstance(zs[0], tf.Tensor):
-            zs = [x.numpy() for x in zs]
-
-        zs = np.array(zs)
         Z = zs.reshape(X.shape)
-
         self.mesh = (X, Y, Z)
 
     def draw_mesh(self, **kwargs):
         self.ax.clear()
         self.ax.plot_surface(self.mesh[0], self.mesh[1], self.mesh[2], **kwargs)
-
-    def draw(self, **kwargs):
-        plt.pause(0.1)
+        self.draw()
+    
+    def draw_point(self, point, color='r'):
+        z = self.function(point).numpy() # get 3d height
+        x,y = point
+        self.ax.scatter(x, y, z, color=color)
+        self.draw()
+    
+    def draw(self, pause_time:float = None, **kwargs):
+        if pause_time:
+            plt.pause(pause_time)
         plt.draw()
--- functions.py
+++ functions.py
@@ -5,31 +5,25 @@ import numpy as np
 
 # === Multi-modal ===
 # D dimensões - Múltiplos mínimos
-def ackley(x, a=20, b=0.2, c=2 * np.math.pi):
-    if isinstance(x, tf.Tensor):
-        d = x.shape.as_list()[0]
-    else:
-        d = len(x)
-    return -a * tf.exp(-b * tf.sqrt(tf.reduce_sum(x * x) / d)) - \
-           tf.exp(tf.reduce_sum(tf.cos(c * x)) / d) + a + np.math.e
+def ackley(x, a=20, b=0.2, c=2*np.math.pi):
+    d = x.shape[0]
+    return -a * tf.exp(-b * tf.sqrt(tf.reduce_sum(x * x, axis=0) / d)) - \
+           tf.exp(tf.reduce_sum(tf.cos(c * x), axis=0) / d) + a + np.math.e
 
 
 # D dimensões - Múltiplos mínimos
 def griewank(x: tf.Tensor):
-    sum = tf.reduce_sum(x ** 2) / 4000.0
+    sum = tf.reduce_sum(x ** 2, axis=0) / 4000.0
     den = tf.range(1, x.shape[0], dtype=x.dtype)
     prod = tf.cos(x / tf.sqrt(den))
-    prod = tf.reduce_prod(prod)
+    prod = tf.reduce_prod(prod, axis=0)
     return sum - prod + 1
 
 
 # D dimensões - Múltiplos mínimos
 def rastrigin(x):
-    if isinstance(x, tf.Tensor):
-        d = x.shape[0]
-    else:
-        d = len(x)
-    return 10 * d + tf.reduce_sum(x ** 2 - 10 * tf.cos(x * 2 * np.math.pi))
+    d = x.shape[0]
+    return 10 * d + tf.reduce_sum(x ** 2 - 10 * tf.cos(x * 2 * np.math.pi), axis=0)
 
 
 def levy(x: tf.Tensor):
@@ -41,7 +35,7 @@ def levy(x: tf.Tensor):
     term3 = (w[d]-1)**2 * (1 + tf.sin(2*pi*w[d])**2)
 
     wi = w[0:d]
-    sum = tf.reduce_sum((wi -1)**2 * (1+ 10*tf.sin(pi*wi+1)**2))
+    sum = tf.reduce_sum((wi -1)**2 * (1+ 10*tf.sin(pi*wi+1)**2), axis=0)
     return term1 + sum + term3
 
 # === Valley-shaped ===
@@ -49,11 +43,7 @@ def levy(x: tf.Tensor):
 def rosenbrock(x):
     x = tf.cast(x, dtype=tf.float32)
     rosen_sum = 0.0
-
-    if isinstance(x, tf.Tensor):
-        d = x.shape[0]
-    else:
-        d = len(x)
+    d = x.shape[0]
 
     for i in range(d - 1):
         rosen_sum += 100 * (x[i + 1] - x[i] ** 2) ** 2 + (x[i] - 1.0) ** 2
@@ -65,14 +55,10 @@ def rosenbrock(x):
 # D dimensões
 def zakharov(x):
     x = tf.cast(x, dtype=tf.float32)
+    d = x.shape[0]
 
-    if isinstance(x, tf.Tensor):
-        d = x.shape[0]
-    else:
-        d = len(x)
-
-    sum1 = tf.reduce_sum(x * x)
-    sum2 = tf.reduce_sum(tf.constant([0.5 * (i + 1) for i in range(d)]) * x)
+    sum1 = tf.reduce_sum(x * x, axis=0)
+    sum2 = tf.reduce_sum(x * tf.range(1, d, dtype=x.dtype)/2, axis=0)
     return sum1 + sum2 ** 2 + sum2 ** 4
 
 
@@ -80,12 +66,7 @@ def zakharov(x):
 # 2 Dimensões
 def bohachevsky(x):
     x = tf.cast(x, dtype=tf.float32)
-
-    if tf.is_tensor(x):
-        d = x.shape[0]
-    else:
-        d = len(x)
-
+    d = x.shape[0]
     assert d == 2
 
     return x[0] ** 2 + 2 * (x[1] ** 2) - 0.3 * tf.cos(3 * np.pi * x[0]) - 0.4 * tf.cos(4 * np.pi * x[1]) + 0.7
@@ -95,53 +76,38 @@ def bohachevsky(x):
 def sum_squares(x):
     x = tf.cast(x, dtype=tf.float32)
 
-    if tf.is_tensor(x):
-        d = x.shape[0]
-    else:
-        d = len(x)
-
-    mul = tf.constant([(i + 1) for i in range(d)], dtype=tf.float32)
-    return tf.reduce_sum((x ** 2) * mul)
+    mul = tf.range(1, x.shape[0], dtype=x.dtype)
+    return tf.reduce_sum((x ** 2) * mul, axis=0)
 
 
 # D dimensões
 def sphere(x):
-    return tf.reduce_sum(x * x)
+    return tf.reduce_sum(x * x, axis=0)
 
 
 # D dimensões:
 def rotated_hyper_ellipsoid(x):
     x = tf.cast(x, dtype=tf.float32)
+    d = x.shape[0]
 
-    if tf.is_tensor(x):
-        d = x.shape[0]
-    else:
-        d = len(x)
-
-    return tf.reduce_sum(tf.convert_to_tensor([tf.reduce_sum(x[0:(i+1)] ** 2) for i in range(d)], dtype=tf.float32))
+    return tf.reduce_sum(tf.convert_to_tensor([tf.reduce_sum(x[0:(i+1)] ** 2, axis=0) for i in range(d)], dtype=tf.float32), axis=0)
 
 
 # === Funções Utilitárias ===
 # Recebe uma função como argumento
 # Retorna o 'limite inferior e superior' da função
 def get_low_and_high(function):
-    if function is sphere:
+    if function is sphere or function is rastrigin:
         return -5.12, 5.12
     elif function is ackley:
         return -32.768, 32.768
-    elif function is rastrigin:
-        return -5.12, 5.12
-    elif function is rosenbrock:
+    elif function is rosenbrock or function is zakharov:
         return -5.0, 10.0
     elif function is bohachevsky:
         return -100.0, 100.0
-    elif function is sum_squares:
+    elif function is sum_squares or function is levy:
         return -10.0, 10.0
-    elif function is zakharov:
-        return -5.0, 10.0
     elif function is griewank:
         return -600.0, 600.0
     elif function is rotated_hyper_ellipsoid:
         return -65.536, 65.536
-    elif function is levy:
-        return -10, 10
