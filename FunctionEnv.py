61697e1ac14518dc78bb05d76a6b614135aa9d72
Moésio Filho <moesiof.dev@gmail.com>
2021-03-10 17:13:30 -0300

    Atualizando arquivos para possuir mesma estrutura utilizada no Colab
    - Atualizando FunctionEnv e learning_to_optimize
    - Adicionando FunctionEnv_TFA_TD3_Model.ipynb

--- FunctionEnv.py
+++ FunctionEnv.py
@@ -2,23 +2,30 @@ import numpy as np
 
 from tf_agents.environments import py_environment
 from tf_agents.specs import array_spec
+from tf_agents.trajectories import time_step as ts
 from collections import namedtuple
 
 hypercube = namedtuple('hypercube', ['min', 'max'])
 
+
 class FunctionEnv(py_environment.PyEnvironment):
-    def __init__(self, function, domain: hypercube(), dims) -> None:
+    def __init__(self, function, domain: hypercube, dims) -> None:
         super().__init__()
         self._function = function
         self._domain = domain
         self._dims = dims
-        self._state = np.random.uniform((dims,))
-        
-        self._action_spec = array_spec.ArraySpec(
-            shape=(dims,), dtype=np.float32, name='action')
+        self._best_solution = np.finfo(np.float32).max
+        self._episode_ended = False
+        self._steps_taken = 0
+        self._state = np.random.uniform(size=(dims,), low=domain.min, high=domain.max) \
+            .astype(dtype=np.float32, copy=False)
+
+        self._action_spec = array_spec.BoundedArraySpec(
+            shape=(dims,), dtype=np.float32,
+            minimum=-1.0, maximum=1.0, name='action')
 
         self._observation_spec = array_spec.BoundedArraySpec(
-            shape=(dims,), dtype=np.float32, 
+            shape=(dims,), dtype=np.float32,
             minimum=domain.min, maximum=domain.max, name='observation')
 
     def action_spec(self):
@@ -27,11 +34,46 @@ class FunctionEnv(py_environment.PyEnvironment):
     def observation_spec(self):
         return self._observation_spec
 
+    def get_info(self):
+        return self._best_solution
+
+    def get_state(self):
+        state = (self._state, self._steps_taken, self._episode_ended)
+        return state
+
+    def set_state(self, state):
+        _state, _steps_taken, _episode_ended = state
+        self._state = _state
+        self._steps_taken = _steps_taken
+        self._episode_ended = _episode_ended
+
     def _step(self, action):
-        pass
+        if self._episode_ended:
+            return self.reset()
+
+        self._state = self._state + action
+        self._state = np.clip(self._state, a_min=self._domain.min, a_max=self._domain.max)
+
+        self._steps_taken += 1
+        if self._steps_taken > 2000:
+            self._episode_ended = True
+
+        reward = -self._function(self._state)
+        if reward < self._best_solution:
+            self._best_solution = reward
+
+        if self._episode_ended:
+            return ts.termination(self._state, reward)
+        else:
+            return ts.transition(self._state, reward)
 
     def _reset(self):
-        pass
+        self._state = np.random.uniform(size=(self._dims,), low=self._domain.min, high=self._domain.max) \
+            .astype(dtype=np.float32, copy=False)
+        self._episode_ended = False
+        self._steps_taken = 0
+        return ts.restart(self._state)
 
-    def render(self):
+    def _render(self):
+        # TODO: Implementar método para renderizar
         pass
--- learning_to_optimize.py
+++ learning_to_optimize.py
@@ -1,122 +1,197 @@
-from Functions_tensorflow import *
-from FunctionDrawer import FunctionDrawer
-from DDPG.DDPG_Agent import DDPGAgent
-
-mpl.use('TkAgg')
-
-
-# Recebe um deque como argumento
-# Retorna os gradientes e f(x)
-def get_gradient(f, position):
-    with tf.GradientTape() as tape:
-        tape.watch(position)
-        loss = f(position)
-    grads = tape.gradient(loss, position)
-    loss = tf.reshape(loss, [])
-    return grads, loss
+from Functions_numpy import *
+from FunctionEnv import *
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tf_agents.environments.wrappers import TimeLimit
+from tf_agents.environments import tf_py_environment
+from tf_agents.agents import Td3Agent
+from tf_agents.policies.policy_saver import PolicySaver
+from tf_agents.agents.ddpg.actor_network import ActorNetwork
+from tf_agents.agents.ddpg.critic_network import CriticNetwork
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.policies.random_tf_policy import RandomTFPolicy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.utils import common
 
 
-# Recebe um deque como argumento
-# Retorna a média dos gradientes normalizada (média não ponderada)
-def get_gradients_mean_normalized(gradients_hist):
-    gradients_hist_tensor = tf.convert_to_tensor([gradients_hist[i] for i in range(len(gradients_hist))])
-    gradients_mean = tf.reduce_mean(gradients_hist_tensor, axis=0)
-    return tf.math.l2_normalize(gradients_mean, axis=0)
+# Função para testar o agente treinado (Plota fitness x iteração)
+def evaluate_agent(eval_env, policy_eval, function, dims):
+    time_step = eval_env.reset()
+
+    pos = time_step.observation.numpy()[0]
+    best_solution = function(pos)
+
+    best_solution_at_it = []
+    best_solution_at_it.append(best_solution)
+    best_it = 0
+    it = 0
+
+    while not time_step.is_last():
+        it += 1
+        action_step = policy_eval.action(time_step)
+        time_step = eval_env.step(action_step.action)
+
+        obj_value = -time_step.reward.numpy()[0]
+
+        if obj_value < best_solution:
+            best_solution = obj_value
+            pos = time_step.observation.numpy()[0]
+            best_it = it
+
+        best_solution_at_it.append(best_solution)
+
+    fig, ax = plt.subplots()
+    ax.plot(range(len(best_solution_at_it)), best_solution_at_it)
+    ax.set(xlabel="Iteration", ylabel="Best objective value",
+           title="TD3 on {0} ({1} Dims)".format(function.__name__, dims))
+    ax.grid()
+    plt.show()
+    print('best_solution: ', best_solution)
+    print('found at it: ', best_it)
+    print('at position: ', pos)
 
 
-# Recebe um deque como argumento
-# Retorna se o agente 'parou' em uma determinada posição
-def has_stopped(position_hist):
-    if len(position_hist) < 5:
-        return False
-    position_hist_tensor = tf.convert_to_tensor([position_hist[i] for i in range(len(position_hist))])
-    mean_pos = tf.reduce_mean(position_hist_tensor)
-    max_pos = tf.reduce_max(position_hist_tensor)
-    min_pos = tf.reduce_min(position_hist_tensor)
-    dif = tf.abs(tf.sqrt(tf.reduce_sum((max_pos - mean_pos) ** 2)) - tf.sqrt(tf.reduce_sum((mean_pos - min_pos) ** 2)))
-    return dif < 0.0001
+# Hiperparametros de treino
+num_episodes = 800  # @param {type:"integer"}
+initial_collect_episodes = 10  # @param {type:"integer"}
+collect_steps_per_iteration = 1  # @param {type:"integer"}
 
+# Hiperparametros da memória de replay
+buffer_size = 1000000  # @param {type:"integer"}
+batch_size = 64  # @param {type:"number"}
 
-def optimize(f, optimizer, training=True, render=False,
-             BEST_SOLUTION=tf.float32.max,
-             EPISODES=10, STEPS=50, LOW=-5.12, HIGH=5.12, DIMS=10):
-    drawer = FunctionDrawer(f, (LOW, HIGH))
-    rewards = []
-    for ep in range(EPISODES):
-        if render:
-            drawer.draw_mesh(alpha=0.5)
+# Hiperparametros do Agente
+actor_lr = 1e-4  # @param {type:"number"}
+critic_lr = 2e-4  # @param {type:"number"}
+tau = 5e-4  # @param {type:"number"}
+discount = 0.99  # @param {type:"number"}
+exploration_noise_std = 0.2  # @param {type:"number"}
+target_policy_noise = 0.2  # @param {type:"number"}
+target_policy_noise_clip = 0.5  # @param {type:"number"}
+actor_update_period = 2  # @param {type:"integer"}
+target_update_period = 2  # @param {type:"integer"}
+reward_scale_factor = 0.75  # @param {type:"number"}
 
-        episode_reward = 0.0
+# --- Arquitetura da rede ---
+# Actor
+fc_layer_params = [400, 300]  # FNN's do Actor
+# Critic
+observation_fc_layer_params = [400]  # FNN's apenas para observações
+joint_fc_layer_params = [300]  # FNN's depois de concatenar (observação, ação)
 
-        position = tf.random.uniform(minval=LOW, maxval=HIGH, shape=(DIMS,))
-        gradients, loss = get_gradient(f, position)
+# Envs
+steps = 500  # @param {type:"integer"}
+steps_eval = 2000  # @param {type:"integer"}
+dims = 2  # @param {type:"integer"}
+function = ackley
+low, high = get_low_and_high(function)
 
-        state = position
+env = FunctionEnv(function=function, domain=hypercube(min=low, max=high), dims=dims)
 
-        for i in range(STEPS):
-            action = optimizer.choose_action(observation=state, current_position=position, training=training)
+env_training = TimeLimit(env=env, duration=steps)
+env_eval = TimeLimit(env=env, duration=steps_eval)
+tf_env_training = tf_py_environment.TFPyEnvironment(environment=env_training)
+tf_env_eval = tf_py_environment.TFPyEnvironment(environment=env_eval)
 
-            new_position = tf.clip_by_value(position + action, clip_value_min=LOW, clip_value_max=HIGH)
-            new_gradients, new_loss = get_gradient(f, new_position)
+obs_spec = tf_env_training.observation_spec()
+act_spec = tf_env_training.action_spec()
+time_spec = tf_env_training.time_step_spec()
 
-            new_state = new_position
-            reward = -new_loss
-            episode_reward += reward
+# Creating networks
+actor_network = ActorNetwork(input_tensor_spec=obs_spec,
+                             output_tensor_spec=act_spec,
+                             fc_layer_params=fc_layer_params,
+                             activation_fn=tf.keras.activations.relu)
+critic_network = CriticNetwork(input_tensor_spec=(obs_spec, act_spec),
+                               observation_fc_layer_params=observation_fc_layer_params,
+                               joint_fc_layer_params=joint_fc_layer_params,
+                               activation_fn=tf.keras.activations.relu,
+                               output_activation_fn=tf.keras.activations.linear)
 
-            # Teste usando máximo iterações como único critério de parada
-            done = (i >= STEPS - 1)
+# Creating agent
+actor_optimizer = tf.keras.optimizers.Adam(learning_rate=actor_lr)
+critic_optimizer = tf.keras.optimizers.Adam(learning_rate=critic_lr)
 
-            if loss < BEST_SOLUTION:
-                BEST_SOLUTION = loss
+agent = Td3Agent(
+    time_step_spec=time_spec,
+    action_spec=act_spec,
+    actor_network=actor_network,
+    critic_network=critic_network,
+    actor_optimizer=actor_optimizer,
+    critic_optimizer=critic_optimizer,
+    target_update_tau=tau,
+    exploration_noise_std=exploration_noise_std,
+    target_policy_noise=target_policy_noise,
+    target_policy_noise_clip=target_policy_noise_clip,
+    actor_update_period=actor_update_period,
+    target_update_period=target_update_period,
+    reward_scale_factor=reward_scale_factor,
+    train_step_counter=tf.Variable(0),
+    gamma=discount)
 
-            if training:
-                optimizer.memorize(state, action, reward, new_state, done)
-                optimizer.learn()
+agent.initialize()
 
-            if render:
-                loss2d = f(tf.convert_to_tensor([position[0], position[1]], dtype=tf.float32))
-                drawer.ax.scatter(position[0], position[1], loss2d, color='r')
-                drawer.draw()
+# Replay buffer
+replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(data_spec=agent.collect_data_spec,
+                                                               batch_size=tf_env_training.batch_size,
+                                                               max_length=buffer_size)
 
-            position = new_position
-            gradients = new_gradients
-            state = new_state
-            loss = new_loss
+# Data Collection (Collect for initial episodes)
+driver = dynamic_step_driver.DynamicStepDriver(env=tf_env_training,
+                                               policy=agent.collect_policy,
+                                               observers=[replay_buffer.add_batch],
+                                               num_steps=collect_steps_per_iteration)
+driver.run = common.function(driver.run)
 
-            if done:
-                break
+initial_collect_driver = dynamic_step_driver.DynamicStepDriver(env=tf_env_training,
+                                                               policy=agent.collect_policy,
+                                                               observers=[replay_buffer.add_batch],
+                                                               num_steps=collect_steps_per_iteration)
 
-        rewards.append(episode_reward)
-        if ep % 10 == 0:
-            print('function: %s episode: %d \n' % (f.__name__, ep),
-                  '\tfinal objective value: %.2f\n' % loss,
-                  '\tepisode reward: %.2f\n' % rewards[-1],
-                  '\taverage reward (last 20 ep): %.2f\n\n' % np.mean(rewards[-20:]))
-            optimizer.save_model(make_dir=True)
+initial_collect_driver.run = common.function(initial_collect_driver.run)
 
-    return BEST_SOLUTION
+for _ in range(initial_collect_episodes):
+    done = False
+    while not done:
+        time_step, _ = initial_collect_driver.run()
+        done = time_step.is_last()
 
+# Creating a dataset
+dataset = replay_buffer.as_dataset(
+    sample_batch_size=batch_size,
+    num_steps=2)
 
-if __name__ == '__main__':
-    # Número de episódios para cada função
-    episodes = 2500
-    # Quantidade de interações realizadas em cada episódio (2000)
-    # OBS:. Durante treino podemos utilizar um número menor de steps
-    steps = 500
-    # Dimensões das funções: 30 **(Testar para maiores dimensões)
-    dims = 30
-    # num_gradients_history = 20 Por hora não vai ser utilizado.
+iterator = iter(dataset)
 
-    agent = DDPGAgent((dims,), dims)
+# Training
+agent.train = common.function(agent.train)
+agent.train_step_counter.assign(0)
+
+for ep in range(num_episodes):
+    done = False
     best_solution = tf.float32.max
+    ep_rew = 0.0
+    while not done:
+        time_step, _ = driver.run()
+        experience, unused_info = next(iterator)
+        agent.train(experience)
 
-    # No caso de uma única função, o agente treina em (episodes*total_training) episódios
-    function = ackley
-    low, high = get_low_and_high(function)
-    agent.set_low_and_high(low, high)
-    best_solution = optimize(function, agent,
-                             BEST_SOLUTION=best_solution,
-                             EPISODES=episodes, STEPS=steps, LOW=low, HIGH=high, DIMS=dims)
+        obj_value = -time_step.reward.numpy()[0]
 
-    agent.save_model(make_dir=True)
-    print('\n--best solution found: %.2f--\n' % best_solution)
+        if obj_value < best_solution and not time_step.is_first():
+            best_solution = obj_value
+
+        ep_rew += -obj_value
+        done = time_step.is_last()
+
+    print('episode = {0} Best solution on episode: {1} Return on episode: {2}'.format(ep, best_solution, ep_rew))
+
+
+evaluate_agent(tf_env_eval, agent.policy, function, dims)
+evaluate_agent(tf_env_eval, agent.collect_policy, function, dims)
+
+tf_policy_saver = PolicySaver(agent.policy)
+tf_policy_collect_saver = PolicySaver(agent.collect_policy)
+
+tf_policy_saver.save('policy')
+tf_policy_collect_saver.save('policy_collect')
