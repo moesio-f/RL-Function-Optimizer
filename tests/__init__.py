b46506fdf26b475b101b19e58e87164a9713aa5a
Moésio Filho <moesiof.dev@gmail.com>
2021-03-17 17:47:38 -0300

    Implementação inicial de multi_agent_test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,3 @@
 __all__ = ["sphere_test", "function_environment_test", "functions_test",
-           "policy_evaluation_test", "td3_inverting_gradients_test"]
+           "policy_evaluation_test", "td3_inverting_gradients_test",
+           "multi_agent_test"]
