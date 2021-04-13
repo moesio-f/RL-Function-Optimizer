b61c6c6cac20968d6bf13100b500bb8b5ac2d8fc
Moésio Filho <moesiof.dev@gmail.com>
2021-04-13 14:35:40 -0300

    Adicionando tests/morela_test.py
    - Versão inicial do MORELA (https://www.scirp.org/pdf/OJOp_2017062814295443.pdf);
    - Testado em algumas funções, resultados similares aos obtidos no paper;

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
 __all__ = ["function_environment_test", "functions_test",
-           "ga_comparison_test", "multi_agent_test", "policy_evaluation_test",
+           "ga_comparison_test", "morela_test",
+           "multi_agent_test", "policy_evaluation_test",
            "reverb_test", "sphere_test", "spreadsheet_test",
            "td3_inverting_gradients_test", "tf_function_env_test"]
