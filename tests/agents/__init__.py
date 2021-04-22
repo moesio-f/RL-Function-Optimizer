f37526f5fa943373e4f4baf0a2721c56f02cb056
Moésio Filho <moesiof.dev@gmail.com>
2021-04-22 17:32:30 -0300

    Atualizando tests/
    - Movendo alguns scripts para outros subdiretórios;

--- tests/agents/__init__.py
+++ tests/agents/__init__.py
@@ -1,4 +1,2 @@
-__all__ = ["policy_evaluation_test", "reinforce_test",
-           "reinforce_w_baseline_test", "reverb_test",
-           "sac_test", "spreadsheet_test",
-           "td3_inverting_gradients_test"]
+__all__ = ["reinforce_test", "reinforce_w_baseline_test",
+           "reverb_test", "sac_test", "td3_inverting_gradients_test"]
--- tests/comparisons/__init__.py
+++ tests/comparisons/__init__.py
@@ -1,1 +1,3 @@
-__all__ = ["ga_comparison_test", "morela_test", "multi_agent_test", "sphere_test"]
+__all__ = ["ga_comparison_test", "morela_test",
+           "multi_agent_test", "policy_evaluation_test",
+           "spreadsheet_test", "sphere_test"]
