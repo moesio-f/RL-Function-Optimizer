8a99335a50395f20dd56776559c2ef9dc85b3446
Gabriel Augusto <gabriel.a07b@gmail.com>
2021-06-18 09:44:57 -0300

    MADDPG movido para agents - multi agent replay buffer em ultis/multi_agent - MultiAgentSimpleEnv adicionada para testar maddpg

--- environments/gym_env.py
+++ environments/gym_env.py
@@ -1,40 +1,112 @@
-from functions.function import Function
+from typing import List
+
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from functions.function import Domain, Function
 from utils.render.function_drawer import FunctionDrawer
+import matplotlib.pyplot as plt
 import numpy as np
 import gym
 from gym.spaces import Box
 
 class MultiAgentFunctionEnv(gym.Env):
-    state: np.ndarray
-    reseted: bool
-
-    def __init__(self, function: Function, dims, n_agents, clip=False):
+    def __init__(self, function: Function, dims: int, n_agents: int, clip=False):
+        state: list[np.ndarray]
+        reseted: bool
         self.func = function
-        self.drawer = FunctionDrawer(function)
+        # self.drawer = FunctionDrawer(function)
         self.dims = dims
         self.n = n_agents
-        self.observation_space = Box(function.domain.min, function.domain.max, (n_agents, dims))
-        self.action_space = self.observation_space if clip else Box(-np.inf, np.inf, (n_agents, dims))
+        self.observation_space = [Box(function.domain.min, function.domain.max, (dims,)) for _ in range(n_agents)]
+        if clip:
+            self.action_space = self.observation_space.copy()
+        else:
+            self.action_space = [Box(-np.inf, np.inf, (dims,)) for _ in range(n_agents)]
 
-    def step(self, action: np.ndarray):
-        self.state += action
-        if not self.observation_space.contains(self.state):
-            self.state = np.clip(self.state, self.func.domain.min, self.func.domain.max)
+
+    def step(self, actions: List[np.ndarray]):
+        min, max = self.func.domain
+        for i in range(len(actions)):
+            self.state[i] += actions[i]
+
+            if not self.observation_space[i].contains(self.state):
+                self.state[i] = np.clip(self.state[i], min, max)
         
-        rewards = -self.func(self.state.T)
+        rewards = -self.func(np.array(self.state).T)
         dones = [False] * self.n  # currently this environment does not know the final pos
 
         return self.state, rewards, dones, None
         
     def reset(self):
-        self.state = np.random.uniform(self.func.domain.min, self.func.domain.max, 
-                                       self.observation_space.shape)
-        self.state = self.state.astype(np.float32)
+        min, max = self.func.domain
+        self.state = [np.random.uniform(min, max, space.shape).astype(np.float32) 
+            for space in self.observation_space]
         self.reseted = True
         return self.state
     
     def render(self, mode='human'):
-        if self.reseted:
-            self.drawer.draw_mesh()
-            self.reseted = False
-        self.drawer.draw_point(self.state.T, pause_time=0.1)
+        pass
+        # if self.reseted:
+        #     self.drawer.draw_mesh()
+        #     self.reseted = False
+        # self.drawer.scatter(self.state.T, pause_time=0.1)
+
+
+class SimpleMultiAgentEnv(gym.Env):
+    def __init__(self, dims: int, n_agents: int = 1, n_landmarks: int = 1, clip=False):
+        self.dims = dims
+        self.n_agents = n_agents
+        self.n_landmarks = n_landmarks
+        self.fig, self.ax = plt.subplots()
+        self.domain = Domain(-1.0, 1.0)
+
+        self.observation_space = [Box(*self.domain, (dims*n_landmarks,)) for _ in range(n_agents)]
+        self.action_space = [Box(*self.domain, (dims,)) for _ in range(n_agents)]
+
+        self.agent_p = self.ax.scatter(0, 0, color='b')
+        self.land_p = self.ax.scatter(0, 0, color='r')
+        self.ax.set_xlim(self.domain)
+        self.ax.set_ylim(self.domain)
+
+    def step(self, actions: List[np.ndarray]):
+        min, max = self.domain
+        for i in range(len(actions)):
+            self.states[i] += actions[i]
+
+            if not self.observation_space[i].contains(self.states):
+                self.states[i] = np.clip(self.states[i], min, max)
+        
+        rewards = [-np.sum((s - l) ** 2) for s,l in zip(self.states, self.landmarks)]
+        dones = [False] * self.n_agents  # currently this environment does not know the final pos
+        observation = self._observation()
+
+        return observation, rewards, dones, None
+        
+    def reset(self):
+        min, max = self.domain
+        shape = (1, self.dims)
+
+        self.states = [np.random.uniform(min, max, shape).astype(np.float32)
+            for _ in range(self.n_agents)]
+
+        self.landmarks = [np.random.uniform(min, max, shape).astype(np.float32)
+            for _ in range(self.n_landmarks)]
+
+        self.render()
+
+        return self._observation()
+    
+    def _observation(self):
+        observations = []
+        for state in self.states:
+            entity_pos = [entity - state for entity in self.landmarks]
+            observations.append(np.concatenate(entity_pos))
+        return observations
+
+    
+    def render(self, mode='human'):
+        agent_p = self.states[0][0]
+        land_p = self.landmarks[0][0]
+
+        self.agent_p.set_offsets(agent_p)
+        self.land_p.set_offsets(land_p)
--- tests/agents/maddpg_test.py
+++ tests/agents/maddpg_test.py
@@ -1,268 +1,27 @@
-from typing import List, Tuple
-
+from matplotlib import pyplot as plt
 import numpy as np
-import tensorflow as tf
-
-from tf_agents.networks import Sequential
-from tf_agents.utils import common
-
-class MultiAgentReplayBuffer(object):
-    def __init__(self, size: int, n_agents: int):
-        """Create Prioritized Replay buffer.
-
-        each data added in this replay buffer is a tuple: 
-        (states, actions, rewards, next_states, dones)
-        """
-        self.n = n_agents
-        self.agents = np.zeros((n_agents, size), dtype=tuple)
-        self.used = 0
-        self.maxsize = int(size)
-        self.next_idx = 0
-
-    def __len__(self):
-        return self.used
-
-    def clear(self):
-        self.agents = np.zeros((self.n, self.maxsize), dtype=tuple)
-        self.used = 0
-        self.next_idx = 0
-
-    # each argument must have shape (n_agents, dimentions)
-    def add(self, obs: list, act: list, rew: list, next_obs: list, dones: List[np.bool]):
-        assert self.n == len(obs) == len(act) == len(rew) == len(next_obs) == len(dones)
-        data = list(zip(obs, act, rew, next_obs, dones))
-
-        for i in range(self.n):
-            self.agents[i, self.next_idx] = data[i]
-        
-        self.next_idx = (self.next_idx + 1) % self.maxsize
-
-        if self.used < self.maxsize:
-            self.used += 1
-
-    def sample_index(self, indexes: List[int]):
-        # per agent info
-        states, actions, rewards, next_states, dones = [], [], [], [], []
-
-        for a in range(self.n):
-            tuples = self.agents[a, indexes]
-            
-            trajectory = ([], [], [], [], [])
-            for t in tuples:
-                for i, info in enumerate(t):
-                    trajectory[i].append(info)
-
-            obs, act, rew, new_obs, done = trajectory
-            states.append(np.array(obs, copy=False))
-            actions.append(np.array(act, copy=False))
-            rewards.append(np.array(rew, copy=False))
-            next_states.append(np.array(new_obs, copy=False))
-            dones.append(np.array(done, copy=False))
-
-        return states, actions, rewards, next_states, dones
-
-    def make_index(self, batch_size):
-        return [np.random.randint(0, self.used) for _ in range(batch_size)]
-
-    # get latest batch_size indexes added and mix them
-    def make_latest_index(self, batch_size):
-        idx = [(self.next_idx - 1 - i) % self.maxsize for i in range(batch_size)]
-        np.random.shuffle(idx)
-        return idx
-
-    def sample(self, batch_size):
-        if batch_size > 0:
-            idxes = self.make_index(batch_size)
-        else:
-            idxes = range(0, self.used)
-        states, actions, next_states, rewards, dones = self.sample_index(idxes)
-        
-        return states, actions, next_states, rewards, dones
-
-    def collect(self):
-        return self.sample(-1)
-
-class Agent:
-    def __init__(self, actor_dims: int, critic_dims: int, n_agents: int,
-                 n_actions: int, agent_idx: int, gamma=0.95, tau=0.01, 
-                 actor_fc_params=(256,256), critic_fc_params=(256,256),
-                 chckpt_dir=None):
-        
-        self.gamma = gamma
-        self.tau = tau
-        self.n_actions = n_actions
-        self.n_agents = n_agents
-        self.name = f'agent_{agent_idx}'
-        self.index = agent_idx
-        self.chckpt_dir = chckpt_dir
-
-        # concatenate layer to pass input to critic network
-        self.concatenate = tf.keras.layers.Concatenate()
-        
-        # config actor network
-        actor_layers = [tf.keras.layers.Dense(units, 'relu') for units in actor_fc_params]
-        actor_layers.append(tf.keras.layers.Dense(n_actions, 'linear'))
-
-        input_spec = tf.TensorSpec((actor_dims,))
-        self.actor = Sequential(actor_layers, input_spec, self.name+'_actor')
-        
-        # config critic network
-        critic_layers = [tf.keras.layers.Dense(units, 'relu') for units in critic_fc_params]
-        critic_layers.append(tf.keras.layers.Dense(1, 'linear'))
-
-        input_spec = tf.TensorSpec((critic_dims + n_agents*n_actions,))
-        self.critic = Sequential(critic_layers, input_spec, self.name+'_critic')
-
-        self.target_actor = self.actor.copy(name=self.name+'_target_actor')
-        self.target_critic = self.critic.copy(name=self.name+'_target_critic')
-
-    def initiate(self):
-        self.actor.create_variables()
-        self.critic.create_variables()
-        self.target_actor.create_variables()
-        self.target_critic.create_variables()
-        
-    def update_targets(self):
-        common.soft_variables_update(self.actor.variables,
-            self.target_actor.variables, self.tau)
-        common.soft_variables_update(self.critic.variables,
-            self.target_critic.variables, self.tau)
-
-    def action(self, obs: tf.Tensor) -> tf.Tensor:
-        return self.actor(obs)[0]
-
-    # input shape for each arg: (n_agents, batch_size, dims) 
-    # output shape: (batch, n_agents*states + n_agents*actions)
-    def preprocess(self, states: list, actions: list):
-        return tf.concat(states + actions, -1)
-    
-    def evaluate(self, states, actions):
-        input = self.preprocess(states, actions)
-        return self.critic(input)[0]
-    
-    def evaluate_target(self, states, actions):
-        input = self.preprocess(states, actions)
-        return self.target_critic(input)[0]
-
-    # train critic based on all states and actions of environment
-    def train_critic(self, states: tf.Tensor, actions: tf.Tensor, target_q: tf.Tensor,
-                     optimizer: tf.keras.optimizers.Optimizer):
-        with tf.GradientTape() as tape:
-            critic_value = self.evaluate(states, actions)
-            critic_loss = tf.square(target_q - critic_value)
-            loss = tf.reduce_mean(critic_loss)
-        optimizer.minimize(loss, self.critic.trainable_variables, tape=tape)
-        return critic_loss
-
-
-    # input shape for each arg: (n_agents, batch_size, dims) 
-    def train_actor(self, states: tf.Tensor, actions: tf.Tensor, optimizer: tf.keras.optimizers.Optimizer):
-        observations = states[self.index]
-        actions = tf.unstack(actions)
-        with tf.GradientTape() as tape:
-            new_actions = self.action(observations)
-
-            # update the new batch of action in the joint actions
-            actions[self.index] = new_actions
-
-            q_value = self.evaluate(states, actions)
-            regularization = tf.reduce_mean(tf.square(new_actions))
-            loss = -tf.reduce_mean(q_value)  + 1e-3 * regularization
-        optimizer.minimize(loss, self.actor.trainable_variables, tape=tape)
-        return loss
-
-    def save_checkpoint(self):
-        pass
-
-class MADDPG:
-    def __init__(self, actor_dims: List[int], 
-            n_agents, n_actions, alpha=0.01, beta=0.01, chkpt_dir='results/maddpg/'):
-        """
-        Arguments:
-          actor_dims: a list of integers with the size of each actor observation
-          critic_dims: a integers with the size of each actor observation
-        """
-        self.agents: List[Agent] = []
-        self.n_agents = n_agents
-        self.n_actions = n_actions
-        self.actor_optimizer = tf.keras.optimizers.Adam(alpha, clipnorm=0.5)
-        self.critic_optimizer = tf.keras.optimizers.Adam(beta, clipnorm=0.5)
-
-        # all critic nets receive all observations of actors
-        critic_dims = sum(actor_dims)
-
-        for index, dims in enumerate(actor_dims):
-            self.agents.append(Agent(dims, critic_dims, n_agents, n_actions, index))
-
-    def initialize(self):
-        for agent in self.agents:
-            agent.initiate()
-
-    # raw obs: tensor with shape = (n_agents, dimentions)
-    def action(self, state) -> np.ndarray:
-        state = tf.convert_to_tensor(state)
-        actions = []
-        for agent, obs in zip(self.agents, state):
-            action = agent.action(obs[None])
-            actions.append(action)
-        return tf.concat(actions, 0).numpy()
-
-    def train(self, experience: Tuple[np.ndarray, ...]):
-        """
-        where transition is a 5-tuple with:
-            (observations, actions, rewards, next_observations, dones)
-        and each observation, actions, etc.. has shape (batch_size, n_agents, dims)
-        """
-        # TODO: 
-        # for states, actions, rewards, new_states, dones in zip(experience):
-
-        states, actions, rewards, new_states, dones = experience
-        
-        dones = [tf.convert_to_tensor(done) for done in dones]
-        states = [tf.convert_to_tensor(state) for state in states]
-        actions = [tf.convert_to_tensor(action) for action in actions]
-        rewards = [tf.convert_to_tensor(reward) for reward in rewards]
-        new_states = [tf.convert_to_tensor(new_state) for new_state in new_states]
-
-        # pass to each agent's target actor is own batch of observations
-        new_actions = [agent.target_actor(new_states[i])[0] for i, agent in enumerate(self.agents)]
-        
-        for i, agent in enumerate(self.agents):
-            # Take critic evaluation from new state and taking new actions
-            new_critic_value = agent.evaluate_target(new_states, new_actions)
-            target = rewards[i] + agent.gamma*new_critic_value
-
-            agent.train_critic(states, actions, target, self.critic_optimizer)
-            agent.train_actor(states, actions, self.actor_optimizer)
-
-            agent.update_targets()
-
-    def save(self):
-        for agent in self.agents:
-            agent.save_checkpoint()
+from tf_agents.policies.policy_saver import PolicySaver
+from utils.multi_agent.replay_bufer import MultiAgentReplayBuffer
+from agents.maddpg import MADDPG
 
 
 from environments.gym_env import MultiAgentFunctionEnv
 from functions.numpy_functions import *
 
-if __name__ == '__main__':
+def main():
     BATCH_SIZE = 32
     N_EPISODES = 10_000
     N_STEPS = 25
-    N_AGENTS = 4
-    DIMS = 3
+    N_AGENTS = 20
+    DIMS = 20
     display = False
 
     FUNCTION = Sphere()
     N_ACTIONS = DIMS
 
-    # Observation dims for actor and critic
-    ACTOR_DIMS = [DIMS] * N_AGENTS  # each actor will see a observation which have length of 'dims'
-    CRITIC_DIMS = sum(ACTOR_DIMS)   # every critic will see observation for every agent and
-
     env = MultiAgentFunctionEnv(FUNCTION, DIMS, N_AGENTS, True)
 
-    maddpg_agents = MADDPG(ACTOR_DIMS, N_AGENTS, N_ACTIONS)
+    maddpg_agents = MADDPG(env.observation_space, env.action_space)
     maddpg_agents.initialize()
 
     memory = MultiAgentReplayBuffer(100_000, N_AGENTS)
@@ -323,3 +82,40 @@ if __name__ == '__main__':
             action = maddpg_agents.action(state)
             next_state, reward, done, _ = env.step(action)
             state = next_state
+
+from environments.gym_env import SimpleMultiAgentEnv, MultiAgentFunctionEnv
+
+def test_simple_env():
+    BATCH_SIZE = 32
+    STEPS = 25
+    UPDATE_RATE = 100
+    display = True
+    total_steps = 0
+    best_score = float('-inf')
+
+    env = SimpleMultiAgentEnv(2)
+    
+    agents = MADDPG(env.observation_space, env.action_space)
+    agents.initialize()
+    memory = MultiAgentReplayBuffer(10000, len(agents))
+
+    for ep in range(10_000):
+        states = env.reset()
+        for step in range(30):
+            if display:
+                plt.pause(0.05)
+                env.render()
+
+            actions = agents.action(states)
+            next_states, rewards, dones, infos = env.step(actions)
+            memory.add(states, actions, rewards, next_states, dones)
+            if (len(memory) > BATCH_SIZE * STEPS and total_steps % UPDATE_RATE == 0):
+                agents.train(memory.sample(BATCH_SIZE))
+            total_steps += 1
+            if step % 10 == 0:
+                print('ep:', ep, 'reward:', rewards)
+    print(rewards)
+
+
+if __name__ == '__main__':
+    test_simple_env()
