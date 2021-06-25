131aca0fa9059c04f61af6ff609b0566cc95c338
Moésio Filho <moesiof.dev@gmail.com>
2021-06-25 11:43:36 -0300

    Atualizando comparisons/comparisons_test
    - Atualizando com as mudanças na classe PyFunctionEnvironment;
    - Agora os agentes compartilham o estado inicial do ambiente;
    - PPO adicionado nos testes;

--- tests/comparisons/comparisons_test.py
+++ tests/comparisons/comparisons_test.py
@@ -1,27 +1,24 @@
 import os
 import tensorflow as tf
 from collections import namedtuple
+import csv
 import matplotlib.pyplot as plt
 from tf_agents.environments.tf_py_environment import TFPyEnvironment
 from tf_agents.environments.wrappers import TimeLimit
 from tf_agents.environments.tf_environment import TFEnvironment
 from tf_agents.policies.tf_policy import TFPolicy
-from tf_agents.policies.random_tf_policy import RandomTFPolicy
 from environments.py_function_environment import PyFunctionEnvironment
-from deap import base
-from deap import creator
-from deap import tools
-from deap import algorithms
 from functions.numpy_functions import *
+from functions.function import Function
 
-MODELS_DIR = os.path.join(os.getcwd(), "../models")
+MODELS_DIR = '../models'
 
 
 class Trajectory(namedtuple('Trajectory', ('list_best_values', 'name', 'best_iteration', 'best_position'))):
     pass
 
 
-def plot_trajectories(trajectories: [Trajectory], dims: int, save_to_file: bool = True):
+def plot_trajectories(trajectories: [Trajectory], function: Function, dims: int, save_to_file: bool = True):
     fig, ax = plt.subplots(figsize=(18.0, 10.0,))
 
     for traj in trajectories:
@@ -31,7 +28,8 @@ def plot_trajectories(trajectories: [Trajectory], dims: int, save_to_file: bool 
            ylabel="Best objective value",
            title="{0} ({1}D)".format(function.name, str(dims)))
 
-    ax.set_xscale('symlog', base=2)
+    ax.set_xscale('symlog', base=10)
+    ax.set_yscale('log', base=10)
     ax.set_xlim(left=0)
 
     ax.legend(loc='upper right')
@@ -45,13 +43,24 @@ def plot_trajectories(trajectories: [Trajectory], dims: int, save_to_file: bool 
     plt.show()
 
 
-def run_episode(tf_eval_env: TFEnvironment, policy: TFPolicy, trajectory_name: str) -> Trajectory:
-    time_step = tf_eval_env.reset()
-    info = tf_eval_env.get_info()
+def write_to_csv(trajectories: [Trajectory], function: Function, dims: int):
+    for trajectory in trajectories:
+        with open(f'{function.name}_{str(dims)}_{trajectory.name}_convergence.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['iteration', 'mean_objective_value'])
+            for it, mean_obj_value in enumerate(trajectory.list_best_values):
+                writer.writerow([it, mean_obj_value])
+
+
+def run_episode(tf_eval_env: TFEnvironment,
+                policy: TFPolicy,
+                trajectory_name: str,
+                function: Function) -> Trajectory:
+    time_step = tf_eval_env.current_time_step()
     done = False
 
-    best_solution = info.objective_value[0]
-    best_pos = info.position[0]
+    best_pos = time_step.observation.numpy()[0]
+    best_solution = function(best_pos)
     best_it = 0
     best_values_at_it = [best_solution]
 
@@ -61,13 +70,13 @@ def run_episode(tf_eval_env: TFEnvironment, policy: TFPolicy, trajectory_name: s
         it += 1
         action_step = policy.action(time_step)
         time_step = tf_eval_env.step(action_step.action)
-        info = tf_eval_env.get_info()
 
-        obj_value = info.objective_value[0]
+        pos = time_step.observation.numpy()[0]
+        obj_value = function(pos)
 
         if obj_value < best_solution:
             best_solution = obj_value
-            best_pos = info.position[0]
+            best_pos = pos
             best_it = it
 
         best_values_at_it.append(best_solution)
@@ -79,92 +88,24 @@ def run_episode(tf_eval_env: TFEnvironment, policy: TFPolicy, trajectory_name: s
                       best_position=best_pos)
 
 
-def create_ga_env(dims: int) -> base.Toolbox:
-    def evalFunction(individual):
-        return function(np.array(individual, dtype=np.float32)),
-
-    creator.create("FitnessMinimum", base.Fitness, weights=(-1.0,))
-    creator.create("Individual", list, fitness=creator.FitnessMinimum)
-
-    toolbox_ = base.Toolbox()
-
-    toolbox_.register("attr_position", np.random.uniform, function.domain.min, function.domain.max)
-
-    toolbox_.register("individual", tools.initRepeat, creator.Individual, toolbox_.attr_position, dims)
-    toolbox_.register("population", tools.initRepeat, list, toolbox_.individual)
-
-    toolbox_.register("evaluate", evalFunction)
-    toolbox_.register("mate", tools.cxTwoPoint)
-    toolbox_.register("mutate", tools.mutFlipBit, indpb=0.05)
-    toolbox_.register("select", tools.selTournament, tournsize=3)
-
-    return toolbox_
-
-
-def run_rl_agent(policy_path: str, trajectory_name: str, num_steps: int, dims: int) -> Trajectory:
-    policy_path = os.path.join(MODELS_DIR, policy_path)
-    policy_path = os.path.join(policy_path, f'{dims}D')
-    policy_path = os.path.join(policy_path, function.name)
-    policy = tf.compat.v2.saved_model.load(policy_path)
-
-    env = PyFunctionEnvironment(function, dims)
+def run_rl_agent(policy: TFPolicy,
+                 trajectory_name: str,
+                 num_steps: int,
+                 function: Function,
+                 dims: int,
+                 initial_time_step) -> Trajectory:
+    # Como as policies já estão treinadas, não tem problema remover o clip da ações.
+    # Relembrar que o ambiente não realiza checagem nas ações, apenas os specs que são diferentes.
+    env = PyFunctionEnvironment(function, dims, clip_actions=False)
     env = TimeLimit(env, duration=num_steps)
+
     tf_eval_env = TFPyEnvironment(environment=env)
+    tf_eval_env._time_step = initial_time_step
 
-    return run_episode(tf_eval_env=tf_eval_env, policy=policy,
-                       trajectory_name=trajectory_name)
-
-
-def run_random_policy(dims: int, num_steps=2000) -> Trajectory:
-    env = PyFunctionEnvironment(function, dims)
-    env = TimeLimit(env, duration=num_steps)
-    tf_eval_env = TFPyEnvironment(environment=env)
-    policy = RandomTFPolicy(time_step_spec=tf_eval_env.time_step_spec(),
-                            action_spec=tf_eval_env.action_spec())
-
-    return run_episode(tf_eval_env=tf_eval_env, policy=policy,
-                       trajectory_name='Random')
-
-
-def run_random_policy_bounded_actions(dims: int, num_steps=2000) -> Trajectory:
-    env = PyFunctionEnvironment(function, dims, clip_actions=True)
-    env = TimeLimit(env, duration=num_steps)
-    tf_eval_env = TFPyEnvironment(environment=env)
-    policy = RandomTFPolicy(time_step_spec=tf_eval_env.time_step_spec(),
-                            action_spec=tf_eval_env.action_spec())
-
-    return run_episode(tf_eval_env=tf_eval_env, policy=policy,
-                       trajectory_name='Random-Bounded-Actions')
-
-
-def run_ea_simple(_toolbox: base.Toolbox, num_ind=300, num_gens=2000) -> Trajectory:
-    pop = _toolbox.population(n=num_ind)
-    hof = tools.HallOfFame(1)
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", np.mean)
-    stats.register("best", np.min)
-
-    pop, log = algorithms.eaSimple(pop, _toolbox, cxpb=0.5, mutpb=0.2, ngen=num_gens,
-                                   stats=stats, halloffame=hof, verbose=False)
-
-    objective_values = log.select('best')
-
-    best_val = objective_values[0]
-    best_values_at_it = [best_val]
-    best_gen = 0
-
-    i = 0
-    for val in objective_values[1:]:
-        i += 1
-        if val < best_val:
-            best_val = val
-            best_gen = i
-        best_values_at_it.append(best_val)
-
-    return Trajectory(list_best_values=best_values_at_it,
-                      name='eaSimple-{0}-individuals'.format(num_ind),
-                      best_iteration=best_gen,
-                      best_position=None)
+    return run_episode(tf_eval_env=tf_eval_env,
+                       policy=policy,
+                       trajectory_name=trajectory_name,
+                       function=function)
 
 
 def get_average_trajectory(trajectories: [Trajectory]):
@@ -185,28 +126,76 @@ def get_average_trajectory(trajectories: [Trajectory]):
 
 
 if __name__ == '__main__':
-    function = Sphere()
     DIMS = 30
     STEPS = 500
     EPISODES = 100
 
-    reinforce_trajectories: [Trajectory] = []
-    sac_trajectories: [Trajectory] = []
-    td3_trajectories: [Trajectory] = []
-    td3_ig_trajectories: [Trajectory] = []
+    for FUNCTION in [Sphere(), Ackley()]:
+        # Como as policies já estão treinadas, não tem problema remover o clip da ações.
+        # Relembrar que o ambiente não realiza checagem nas ações, apenas os specs que são diferentes.
+        ENV = PyFunctionEnvironment(FUNCTION, DIMS, clip_actions=False)
+        ENV = TimeLimit(ENV, duration=STEPS)
+        TF_ENV = TFPyEnvironment(environment=ENV)
 
-    for _ in range(EPISODES):
-        reinforce_trajectories.append(run_rl_agent(policy_path='REINFORCE-BL',
-                                                   trajectory_name='Reinforce', num_steps=STEPS, dims=DIMS))
-        sac_trajectories.append(run_rl_agent(policy_path='SAC-AAT',
-                                             trajectory_name='SAC', num_steps=STEPS, dims=DIMS))
-        td3_trajectories.append(run_rl_agent(policy_path='TD3',
-                                             trajectory_name='TD3', num_steps=STEPS, dims=DIMS))
-        td3_ig_trajectories.append(run_rl_agent(policy_path='TD3-IG',
-                                                trajectory_name='TD3-IG', num_steps=STEPS, dims=DIMS))
+        reinforce_policy = tf.compat.v2.saved_model.load(
+            os.path.join(MODELS_DIR, 'REINFORCE-BL/' + f'{DIMS}D/' + FUNCTION.name))
+        reinforce_trajectories: [Trajectory] = []
 
-    plot_trajectories(
-        [get_average_trajectory(reinforce_trajectories),
-         get_average_trajectory(sac_trajectories),
-         get_average_trajectory(td3_trajectories),
-         get_average_trajectory(td3_ig_trajectories)], dims=DIMS)
+        sac_policy = tf.compat.v2.saved_model.load(os.path.join(MODELS_DIR, 'SAC-AAT/' + f'{DIMS}D/' + FUNCTION.name))
+        sac_trajectories: [Trajectory] = []
+
+        td3_policy = tf.compat.v2.saved_model.load(os.path.join(MODELS_DIR, 'TD3/' + f'{DIMS}D/' + FUNCTION.name))
+        td3_trajectories: [Trajectory] = []
+
+        td3_ig_policy = tf.compat.v2.saved_model.load(os.path.join(MODELS_DIR, 'TD3-IG/' + f'{DIMS}D/' + FUNCTION.name))
+        td3_ig_trajectories: [Trajectory] = []
+
+        ppo_policy = tf.compat.v2.saved_model.load(os.path.join(MODELS_DIR, 'PPO-CLIP/' + f'{DIMS}D/' + FUNCTION.name))
+        ppo_trajectories: [Trajectory] = []
+
+        for _ in range(EPISODES):
+            initial_time_step = TF_ENV.reset()
+
+            reinforce_trajectories.append(run_rl_agent(policy=reinforce_policy,
+                                                       trajectory_name='REINFORCE',
+                                                       num_steps=STEPS,
+                                                       function=FUNCTION,
+                                                       dims=DIMS,
+                                                       initial_time_step=initial_time_step))
+
+            sac_trajectories.append(run_rl_agent(policy=sac_policy,
+                                                 trajectory_name='SAC',
+                                                 num_steps=STEPS,
+                                                 function=FUNCTION,
+                                                 dims=DIMS,
+                                                 initial_time_step=initial_time_step))
+
+            td3_trajectories.append(run_rl_agent(policy=td3_policy,
+                                                 trajectory_name='TD3',
+                                                 num_steps=STEPS,
+                                                 function=FUNCTION,
+                                                 dims=DIMS,
+                                                 initial_time_step=initial_time_step))
+
+            td3_ig_trajectories.append(run_rl_agent(policy=td3_ig_policy,
+                                                    trajectory_name='TD3-IG',
+                                                    num_steps=STEPS,
+                                                    function=FUNCTION,
+                                                    dims=DIMS,
+                                                    initial_time_step=initial_time_step))
+
+            ppo_trajectories.append(run_rl_agent(policy=td3_ig_policy,
+                                                 trajectory_name='PPO',
+                                                 num_steps=STEPS,
+                                                 function=FUNCTION,
+                                                 dims=DIMS,
+                                                 initial_time_step=initial_time_step))
+
+        avg_reinforce = get_average_trajectory(reinforce_trajectories)
+        avg_sac = get_average_trajectory(sac_trajectories)
+        avg_td3 = get_average_trajectory(td3_trajectories)
+        avg_td3_ig = get_average_trajectory(td3_ig_trajectories)
+        avg_ppo = get_average_trajectory(ppo_trajectories)
+
+        plot_trajectories([avg_reinforce, avg_sac, avg_td3, avg_td3_ig, avg_ppo], function=FUNCTION, dims=DIMS)
+        write_to_csv([avg_reinforce, avg_sac, avg_td3, avg_td3_ig, avg_ppo], function=FUNCTION, dims=DIMS)
