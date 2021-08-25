789dd26ddf6a793b96e008e13d245464ccb24ae6
Gabriel Barbosa <gabriel.a07b@gmail.com>
2021-08-25 17:54:08 -0300

    Simplificando experimento do MADDPG

--- experiments/training/maddpg.py
+++ experiments/training/maddpg.py
@@ -1,3 +1,4 @@
+import matplotlib
 import numpy as np
 import gym
 import json
@@ -6,12 +7,12 @@ import os
 from matplotlib import pyplot as plt
 
 from src.multi_agent.maddpg import MADDPG
-from src.multi_agent.multi_agent_env import MultiAgentFunctionEnv
+from src.multi_agent.gym_env import MultiAgentFunctionEnv
 from src.multi_agent.replay_buffer import MultiAgentReplayBuffer
 from src.functions.numpy_functions import Sphere
 
 
-def get_result_dir(folder_name='results', index=None):
+def make_results_dir(directory='results'):
   """Get result directory, if doesn't exist create one
     Args:
       index: if None create the next index folder to hold results
@@ -22,66 +23,84 @@ def get_result_dir(folder_name='results', index=None):
     Returns: a tuple with root and models directory
     """
 
-  def get_latest_index_dir(dir):
-    index = 1
-    while os.path.exists(os.path.join(dir, str(index))):
+  index = 1
+  while os.path.exists(os.path.join(directory, str(index))):
+    index += 1
+  directory = os.path.join(directory, str(index), 'models')
+  os.makedirs(directory, exist_ok=True)
+  return os.path.dirname(directory)
+
+def eval(env: gym.Env, agents: MADDPG, episodes=10, steps=50, index=None, directory = 'results'):
+  
+  if index is None:
+    index = 0
+    while os.path.exists(os.path.join(directory, str(index + 1))):
       index += 1
-    return index
 
-  if index:
-    root_dir = os.path.join(folder_name, str(index))
-  else:
-    index = get_latest_index_dir(folder_name)
-    root_dir = os.path.join(folder_name, str(index))
-  models_dir = os.path.join(root_dir, 'models')
-  os.makedirs(models_dir, exist_ok=True)
-  return root_dir, models_dir
+  agents.load(os.path.join(directory, str(index), 'models'))
+  best_score = -np.inf
 
+  for ep in range(episodes):
+    states = env.reset()
+    best_agent = -np.inf
+
+    for step in range(steps):
+      plt.pause(0.05)
+      env.render()
+
+      actions = agents.action(states)
+      next_states, rewards, _, _ = env.step(actions)
+      states = next_states
+
+      best_agent_idx = np.argmax(rewards)
+      best_agent = max(best_agent, rewards[best_agent_idx])
+      best_score = max(best_score, best_agent)
+
+    print(f'episode {ep} | best score: {best_score:.5f} | current best: {best_agent:.5f} by agent {best_agent_idx}')
+  return best_score
 
 # Multi Agent Run
-def run(env: gym.Env, agents: MADDPG, memory: MultiAgentReplayBuffer,
+def train(env: gym.Env, agents: MADDPG, memory: MultiAgentReplayBuffer,
         batch_size=256, episodes=60_000,
-        steps=25, update_rate=100, display=False, save_interval=200,
-        save_index=None):
-  root_dir, models_dir = get_result_dir(index=save_index)
-  with open(os.path.join(root_dir, 'specs.json'), 'w') as specs_file:
-    specs = {'batch_size': batch_size, 'episodes': episodes, 'steps': steps,
-             'update_rate': update_rate, 'num_agents': agents.num_agents}
-    json.dump(specs, specs_file, indent=True)
+        steps=25, update_rate=100, save_interval=200):
+
+  save_directory = make_results_dir()
+  models_dir = os.path.join(save_directory, 'models')
+
+  with open(os.path.join(save_directory, 'specs.json'), 'w') as specs_file:
+    json.dump({
+      'batch_size': batch_size, 
+      'episodes': episodes, 
+      'steps': steps,
+      'update_rate': update_rate,
+      'num_agents': agents.num_agents
+    }, specs_file, indent=True)
 
   total_steps = 0
   best_score = -np.inf
 
-  if save_index is not None:
-    agents.load(models_dir)
-
   for ep in range(episodes):
     states = env.reset()
-    current_best = -np.inf
+    best_agent = -np.inf
 
     for step in range(steps):
-      if display:
-        plt.pause(0.05)
-        env.render()
-
       actions = agents.action(states)
       next_states, rewards, dones, infos = env.step(actions)
       memory.add(states, actions, rewards, next_states, dones)
       states = next_states
 
-      if len(
-            memory) > batch_size * steps and total_steps % update_rate == 0 and not display:
+      if len(memory) > batch_size * steps and total_steps % update_rate == 0:
         experience = memory.sample(batch_size)
         losses = agents.train(experience)
 
-      current_best = max(current_best, np.max(rewards))
-      best_score = max(best_score, current_best)
+      best_agent_idx = np.argmax(rewards)
+      best_agent = max(best_agent, rewards[best_agent_idx])
+      best_score = max(best_score, best_agent)
 
       total_steps += 1
-    print(
-      f'episode {ep} | best score: {best_score:.5f} | current best: {current_best:.5f}')
+    print(f'episode {ep} | best score: {best_score:.5f} | current best: {best_agent:.5f} by agent {best_agent_idx}')
 
-    if not display and ep % save_interval == 0:
+    if ep % save_interval == 0:
       agents.save(models_dir)
   return best_score
 
@@ -111,20 +130,19 @@ class ScriptedAgent:
 
 
 def test_env(evaluate=False, save_index=None):
-  env = MultiAgentFunctionEnv(Sphere(), 2, 1)
+  env = MultiAgentFunctionEnv(Sphere(), dims=2, n_agents=1)
+  
   # env = SimpleMultiAgentEnv(2)
   agents = MADDPG(env.observation_space, env.action_space)
-  # agents = ScriptedAgent()
-  memory = MultiAgentReplayBuffer(1_000_000, len(agents))
   agents.initialize()
+  # agents = ScriptedAgent()
   if evaluate:
-    best = run(env, agents, memory, episodes=10, display=True,
-               save_index=save_index)
+    best = eval(env, agents, index=save_index)
   else:
-    best = run(env, agents, memory, steps=25, episodes=5_000, update_rate=64,
-               save_index=save_index)
+    memory = MultiAgentReplayBuffer(1_000_000, len(agents))
+    best = train(env, agents, memory, steps=25, episodes=5_000, update_rate=64)
   print('Best result:', best)
 
-
 if __name__ == '__main__':
-  test_env()
+  test_env(evaluate=False)
+
