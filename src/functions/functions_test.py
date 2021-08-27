28712d9753b5a696a1c308605b89b685bc5b5db4
Gabriel Barbosa <gabriel.a07b@gmail.com>
2021-08-27 12:52:48 -0300

    testes unitarios das funções em numpy

--- src/functions/functions_test.py
+++ src/functions/functions_test.py
@@ -1,12 +1,75 @@
 """Function validation tests."""
 
 import tensorflow as tf
+import numpy as np
+import unittest
 
 from src.functions import numpy_functions as npf
 from src.functions import tensorflow_functions as tff
 
 
-if __name__ == '__main__':
+class TestNumpyFunctions(unittest.TestCase):
+  
+  def setUp(self) -> None:
+    self.array = np.array([1,2,3,4], dtype=np.float32)
+    self.zero = np.array([0,0,0,0], dtype=np.float32)
+
+  def tearDown(self) -> None:
+    del self.array
+    del self.zero
+
+  def test_ackley(self):
+    f = npf.Ackley()
+    result = f(self.array)
+    self.assertAlmostEqual(result, 8.4346944444)
+  
+  def test_griewank(self):
+    f = npf.Griewank()
+    result = f(self.array)
+    self.assertAlmostEqual(result, 1.0018703780)
+  
+  def test_rastrigin(self):
+    f = npf.Rastrigin()
+    result = f(self.array)
+    self.assertAlmostEqual(result, 30.0000000000)
+
+  def test_levy(self):
+    f = npf.Levy()
+    result = f(self.array)
+    self.assertAlmostEqual(result, 2.7639718055725098)
+
+  def test_rosenbrock(self):
+    f = npf.Rosenbrock()
+    result = f(self.array)
+    self.assertAlmostEqual(result, 2705.0000000000)
+
+  def test_zakharov(self):
+    f = npf.Zakharov()
+    result = f(self.array)
+    self.assertAlmostEqual(result, 50880.0000000000)
+
+  def test_sum_squares(self):
+    f = npf.SumSquares()
+    result = f(self.array)
+    self.assertAlmostEqual(result, 100.0000000000)
+  
+  def test_sphere(self):
+    f = npf.Sphere()
+    result = f(self.array)
+    self.assertAlmostEqual(result, 30.0000000000)
+
+  def test_rotated_hyper_ellipsoid(self):
+    f = npf.RotatedHyperEllipsoid()
+    result = f(self.array)
+    self.assertAlmostEqual(result, 50.0000000000)
+
+  def test_dixon_price(self):
+    f = npf.DixonPrice()
+    result = f(self.array)
+    self.assertAlmostEqual(result, 4230.0000000000)
+
+
+def test_random():
   list_tf_functions = tff.list_all_functions()
   list_np_functions = npf.list_all_functions()
 
@@ -33,3 +96,6 @@ if __name__ == '__main__':
     print('----------------------------')
 
   print('Process finished.')
+ 
+if __name__ == "__main__":
+  unittest.main()
